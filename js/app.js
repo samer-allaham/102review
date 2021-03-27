b77c0b3fe8bf99111cac72c426c38f0b5edb1ce2
samer <s.allaham@ltuc.com>
2021-03-27 16:21:51 +0300

    finished review

---
+++ js/app.js
@@ -0,0 +1,112 @@
+// alert('working?')
+
+
+
+// comment in js for one line 
+/*
+functions :
+
+function declaration:
+
+function funcName(parameters){
+    
+    return output
+    
+}
+funcName(arguments);
+
+
+
+function expression:
+
+var funcName= function(parameters){
+    code
+}
+
+funcName(arguments);
+
+*/
+
+// console.log('before', car);
+// car = 'bmw';
+// console.log('after', car);
+
+// var car;
+
+
+// welcomeMessage();
+
+// function welcomeMessage() {
+//     console.log('hello there from function declaraion');
+// }
+
+
+
+// var getAge = function (age) {
+//     // age=30
+//     console.log('my age is ' + age);
+// }
+
+// getAge(30);
+// getAge(40);
+// getAge('20');
+
+
+
+// var makeSandwich = function (bread, meat, cheese) {
+//     var sandwich = '';
+
+//     sandwich = sandwich +  bread  ;
+//     sandwich = sandwich +  meat ;
+//     sandwich = sandwich +  cheese ;
+//     sandwich = sandwich +  bread ;
+
+//     console.log(sandwich); 
+// }
+// // makeSandwich(cheese, meat, bread);
+// makeSandwich('rye', 'pastrami', 'provalone');
+// makeSandwich('shrak', 'turkey', 'abu alwalad');
+
+// var userName=prompt('whats your name');
+
+
+// function welcomeMessage() {
+//     console.log('Welcome to my page '+userName);
+// }
+
+// welcomeMessage();
+
+
+// var i = 0;
+// while( i <= 10 ) {
+//   console.log(++i);
+//   i++;
+// }
+
+
+// var tired=true;
+// var hungrey=false;
+
+// while (tired||hungrey) {
+//     if (hungrey==false){
+//         tired=false
+
+//         console.log('im hungrey');
+//     }
+//     else{
+//         console.log('im good');
+//     }
+// }
+var i; 
+for (i = 0; i == 0 || i < 8; i++) {
+  console.log(i);
+}
+
+
+
+
+var c = 'o';
+while( c.length <= 20 ) {
+  console.log( c );
+  c = c + c;
+}
